6240dd4b77fb815216fa9e134322bc33916f2410
Poeticghostwolf <shaylestylez@gmail.com>
2026-04-12 15:00:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Kiaroni 🚀</h1>
+      <p>It finally works.</p>
+    </div>
+  );
+}
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
